54540f176d757464422776183b3d502e02cc1b4e
Mahbuba Islam Priya <mahbubacse18@gmail.com>
2025-03-14 22:51:49 +0600

    first commit

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,76 @@
+import Lottie from "lottie-react";
+import React from "react";
+import RegisterLottiData from "../../assets/Lotti/register.json";
+
+const Register = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left w-96">
+          <Lottie animationData={RegisterLottiData}></Lottie>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="ml-8 mt-4 text-5xl font-bold">Register now!</h1>
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Enter your photo"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              {/* <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label> */}
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary w-full">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
